87287818ea15c3a4163f4d656f49b9eceffceada
mattphillips <mattphillips@mail.com>
2019-08-17 17:53:30 +0100

    WIP map streaming

--- TOOLS/BeehivePlugins/luminary/MapExporter.cpp
+++ TOOLS/BeehivePlugins/luminary/MapExporter.cpp
@@ -8,6 +8,8 @@
 
 #include "MapExporter.h"
 
+#include <ion/core/memory/Endian.h>
+
 namespace luminary
 {
 	bool MapExporter::ExportMap(const std::string& binFilename, const Map& map, int stampWidth, int stampHeight)
@@ -17,18 +19,21 @@ namespace luminary
 		{
 			int widthStamps = map.GetWidth() / stampWidth;
 			int heightStamps = map.GetHeight() / stampHeight;
+			u32 stampSizeBytes = stampWidth * stampHeight * 2;
 
-			std::vector<u8> stampMap;
+			std::vector<u32> stampMap;
 			stampMap.resize(widthStamps * heightStamps);
 
 			for (TStampPosMap::const_iterator it = map.StampsBegin(), end = map.StampsEnd(); it != end; ++it)
 			{
 				int x = it->m_position.x / stampWidth;
 				int y = it->m_position.y / stampHeight;
-				stampMap[(y * widthStamps) + x] = it->m_id;
+				u32 word = (it->m_id * stampSizeBytes);
+				ion::memory::EndianSwap(word);
+				stampMap[(y * widthStamps) + x] = word;
 			}
 
-			file.Write(stampMap.data(), stampMap.size());
+			file.Write(stampMap.data(), stampMap.size() * sizeof(u32));
 			file.Close();
 			return true;
 		}
